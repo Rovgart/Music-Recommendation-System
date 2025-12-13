6b9e01766fb1bb7f8aa9a666fa535093326f380d
Daniel Niedzielski <daniel.niedziels@wp.pl>
2025-12-13 02:00:23 +0100

    feat: zustand auth store changes

--- ai-music-recommendation-app/app/index.tsx
+++ ai-music-recommendation-app/app/index.tsx
@@ -1,7 +1,14 @@
+import { useAuthStore } from "@/store/authStore";
+import { useEffect } from "react";
 import { View } from "react-native";
 import { useTheme } from "react-native-paper";
 import BottomNav from "./components/navigation/BottomNavigation";
 export default function Index() {
+	const initAuthListener = useAuthStore((s) => s.initAuthListener);
+	useEffect(() => {
+		const unsubscribe = initAuthListener();
+		return unsubscribe;
+	}, [initAuthListener]);
 	const theme = useTheme();
 
 	return (
--- ai-music-recommendation-app/store/authStore.ts
+++ ai-music-recommendation-app/store/authStore.ts
@@ -1,61 +1,22 @@
-import { userService } from "@/api/UserService";
-import { LoginRequestT, User } from "@/types";
-import * as Keychain from "react-native-keychain";
+import { auth } from "@/firebase";
+import { onAuthStateChanged, type User } from "firebase/auth";
 import { create } from "zustand";
-interface AuthStoreI {
-  isAuthenticated: boolean;
-  token: null | string;
-  user: User;
-  setIsAuthenticated: (isAuth: boolean) => void;
-  setToken: (token: string) => void;
-  setUser: (userData: User) => void;
-  login: (userData: LoginRequestT) => Promise<void>;
-  logout: () => void;
-}
-export const useAuthStore = create<AuthStoreI>((set, get) => ({
-  isAuthenticated: false,
-  token: null,
-  user: {
-    id: 1,
-    username: "",
-    email: "",
-  },
-  setIsAuthenticated: (isAuth) => set({ isAuthenticated: isAuth }),
-  setToken: async (token) => {
-    await Keychain.setInternetCredentials(
-      "aimusicrecommendationapp_auth",
-      "ACCESS_TOKEN",
-      token
-    );
-  },
-  getToken: async () => {
-    try {
-      const credentials = await Keychain.getInternetCredentials(
-        "aimusicrecommendationapp_auth"
-      );
-      return credentials ? credentials.password : null;
-    } catch (error) {
-      return null;
-    }
-  },
-  setUser: (userData) =>
-    set({
-      user: userData,
-      isAuthenticated: true,
-    }),
-  login: async (userData) => {
-    const { setUser, setToken, setIsAuthenticated } = get();
-    const res = await userService.loginUser(userData);
-    if (res?.access_token) {
-      setIsAuthenticated(true);
-      setToken(res?.access_token);
-      setUser(res?.user);
-    }
-  },
-  logout: () =>
-    set(() => ({
-      isAuthenticated: false,
-      token: null,
-      user: { id: null, username: "" },
-    })),
+
+type AuthState = {
+	user: User | null;
+	loading: boolean;
+	initAuthListener: () => void;
+};
+export const useAuthStore = create<AuthState>((set, get) => ({
+	user: null,
+	loading: true,
+	initAuthListener: () => {
+		const unsubscribe = onAuthStateChanged(auth, (user) => {
+			set({
+				user,
+				loading: false,
+			});
+		});
+		return unsubscribe;
+	},
 }));
